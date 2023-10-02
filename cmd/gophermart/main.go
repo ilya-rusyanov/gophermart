c88ba583e46aa98a5970b40b619c6196d6b0f509
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-10-02 19:42:13 +0300

    logger

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ilya-rusyanov/gophermart/internal/config"
+	"github.com/ilya-rusyanov/gophermart/internal/logger"
 )
 
 func main() {
 	config := config.New()
 	config.Parse()
+
+	logger, err := logger.New(config.LogLevel)
+	if err != nil {
+		fmt.Printf("failed to initialize logger: %q\n", err)
+		os.Exit(1)
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,6 +9,7 @@ type Config struct {
 	ListenAddr  string
 	DSN         string
 	AccrualAddr string
+	LogLevel    string
 }
 
 func New() *Config {
@@ -16,6 +17,7 @@ func New() *Config {
 	flag.StringVar(&res.ListenAddr, "a", ":8080", "address and port to listen on")
 	flag.StringVar(&res.DSN, "d", "host=localhost port=5433 user=gophermart dbname=gophermart sslmode=disable", "database URI")
 	flag.StringVar(&res.AccrualAddr, "r", "localhost:8081", "accrual system address")
+	flag.StringVar(&res.LogLevel, "-l", "info", "log level")
 	return &res
 }
 
@@ -32,5 +34,9 @@ func (c *Config) Parse() {
 
 	if val := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); len(val) > 0 {
 		c.AccrualAddr = val
+	}
+
+	if val := os.Getenv("LOG_LEVEL"); len(val) > 0 {
+		c.LogLevel = val
 	}
 }
