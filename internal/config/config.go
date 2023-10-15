e364f8d1dbb3993177bd7ade62554d0e29ab599e
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-10-15 16:08:17 +0300

    fix flag parsing

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,7 +17,7 @@ func New() *Config {
 	flag.StringVar(&res.ListenAddr, "a", ":8080", "address and port to listen on")
 	flag.StringVar(&res.DSN, "d", "host=localhost port=5433 user=gophermart dbname=gophermart sslmode=disable", "database URI")
 	flag.StringVar(&res.AccrualAddr, "r", "localhost:8081", "accrual system address")
-	flag.StringVar(&res.LogLevel, "-l", "info", "log level")
+	flag.StringVar(&res.LogLevel, "l", "info", "log level")
 	return &res
 }
 
