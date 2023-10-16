be1a79c79eed16dd32b83afdb4fb9e361eb7e052
Ilya Rusyanov <rusyanov@monitorsoft.ru>
2023-10-16 17:07:29 +0300

    order table migration

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -31,5 +31,21 @@ func migrate(ctx context.Context, db *sql.DB) error {
 	if err != nil {
 		return fmt.Errorf("failed to create users table: %w", err)
 	}
+
+	_, err = db.ExecContext(ctx,
+		`CREATE TYPE order_state AS ENUM (
+'new', 'registered', 'invalid', 'processing', 'processed')`)
+	if err != nil {
+		return fmt.Errorf("failed to create order state enumeration: %w", err)
+	}
+
+	_, err = db.ExecContext(ctx,
+		`CREATE TABLE IF NOT EXISTS orders
+(id int PRIMARY KEY, username text NOT NULL, upload_time timestamptz NOT NULL,
+state order_state NOT NULL, value int)`)
+	if err != nil {
+		return fmt.Errorf("failed to create order table: %w", err)
+	}
+
 	return nil
 }
