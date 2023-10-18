6c81eda3527de169d3ea70550b46b010a5a97c4e
Ilya Rusyanov <rusyanov@monitorsoft.ru>
2023-10-18 16:47:59 +0300

    change default accrual URI

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ func New() *Config {
 	var res Config
 	flag.StringVar(&res.ListenAddr, "a", ":8080", "address and port to listen on")
 	flag.StringVar(&res.DSN, "d", "host=localhost port=5433 user=gophermart dbname=gophermart sslmode=disable", "database URI")
-	flag.StringVar(&res.AccrualAddr, "r", "localhost:8081", "accrual system address")
+	flag.StringVar(&res.AccrualAddr, "r", "http://localhost:8082", "accrual system address")
 	flag.StringVar(&res.LogLevel, "l", "info", "log level")
 	return &res
 }
