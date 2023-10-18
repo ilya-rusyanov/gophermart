b0bf98daab22e8f7be89181a0abb1dfc755dda99
Ilya Rusyanov <rusyanov@monitorsoft.ru>
2023-10-18 17:53:22 +0300

    WIP

--- internal/adapters/accrual/accrual.go
+++ internal/adapters/accrual/accrual.go
@@ -3,6 +3,7 @@ package accrual
 import (
 	"context"
 	"errors"
+	"net/http"
 
 	"github.com/ilya-rusyanov/gophermart/internal/entities"
 )
@@ -21,5 +22,6 @@ func (a *Adapter) GetStateOfOrder(ctx context.Context, orderID entities.OrderID)
 	entities.OrderStatus, error,
 ) {
 	var currentStatus entities.OrderStatus
+	resp, err := http.Get(a.addr)
 	return currentStatus, errors.New("TODO")
 }
