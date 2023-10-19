03dd699c948ebad4de46fb525605b1c230034294
Ilya Rusyanov <rusyanov@monitorsoft.ru>
2023-10-19 14:58:31 +0300

    order id is string type when listing

--- internal/entities/order_list.go
+++ internal/entities/order_list.go
@@ -12,7 +12,7 @@ const (
 )
 
 type Order struct {
-	ID         OrderID     `json:"number"`
+	ID         string      `json:"number"`
 	Status     OrderStatus `json:"status"`
 	Accrual    *Currency   `json:"accrual,omitempty"`
 	UploadedAt time.Time   `json:"uploaded_at"`
